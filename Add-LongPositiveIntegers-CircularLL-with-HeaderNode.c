8201f69dddb069043eb96f9aaffaf8f60e67fe4f
Faizan-Mushtaq <42917696+Faizan-Mushtaq@users.noreply.github.com>
2019-02-25 21:03:29 +0530

    Add files via upload

---
+++ Add-LongPositiveIntegers-CircularLL-with-HeaderNode.c
@@ -0,0 +1,128 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+      int data;
+      struct node *next;
+};
+struct node *getnode();
+struct node *insert(struct node *,int);
+struct node *add(struct node *,struct node *);
+void display (struct node *);
+
+int main()
+{
+  int i,value;
+  char str1[100],str2[100];
+  struct node *first=NULL,*second=NULL,*res=NULL;
+  first = getnode();
+  first->data=0;
+  first->next=first;//header node
+  second=getnode();
+  second->data=0;
+  second->next=second;
+  printf("ENTER FIRST NO\n");
+  scanf("%s",str1);
+  printf("ENTER SECOND NO\n");
+  scanf("%s",str2);
+  for(i=0;str1[i]!='\0';i++)
+  {
+    value=str1[i]-48;
+    first=insert(first,value);
+  }
+  printf("FIRST NUMBER IS:\n");
+  puts(str1);//display(first);because prints backwards
+  for(i=0;str2[i]!='\0';i++)
+  {
+    value=str2[i]-48;
+    second=insert(second,value);//inserts front that is why prints backwards.
+  }
+  printf("SECOND NUMBER IS:\n");
+  puts(str2);//display(second);
+  res=add(first,second);
+  printf("ANSWER IS:\n");
+  display(res);
+  return 0;
+}
+
+struct node *getnode()
+{
+  struct node *newnode;
+  newnode=(struct node *)malloc(sizeof(struct node));
+  newnode->next =newnode; //circular list
+  return newnode;
+}
+struct node * insert(struct node *head,int value)
+{
+  struct node *newnode;
+  newnode=(struct node *)malloc(sizeof(struct node));//or getnode()
+  newnode->data=value;
+  if(head->next==head)
+  {
+    head->next=newnode;
+    newnode->next=head;
+  }else{
+    newnode->next=head->next;
+    head->next=newnode;
+  }
+  return head;
+}
+struct node *add(struct node *first,struct node *second)
+{
+    struct node *res=NULL,*temp,*prev=NULL,*h1,*h2;
+    int carry=0,sum=0;
+    res=getnode();
+    res->data=0;
+    res->next=res;//header node
+    h1=first;
+    h2=second;
+    first=first->next;
+    second=second->next;//to leave header
+    while(first!=h1 && second!=h2)
+      {
+        sum=carry+(first?first->data:0)+(second?second->data:0);
+        carry=(sum>=10)?1:0;
+        sum=sum%10;
+        res=insert(res,sum);
+        if(first!=h1){first=first->next;}
+        if(second!=h2){second=second->next;}
+      }
+      if(first!=h1)
+      {
+        while(first!=h1)
+        {
+          sum=carry+first->data;
+          carry=(sum>=10)?1:0;
+          sum=sum%10;
+          res=insert(res,sum);
+          if(first!=h1){first=first->next;}
+        }
+      }
+      if(second!=h2)
+      {
+        while(second!=h2)
+        {
+          sum=carry+second->data;
+          carry=(sum>=10)?1:0;
+          sum=sum%10;
+          res=insert(res,sum);
+          if(second!=h2){second=second->next;}
+        }
+      }
+      if(carry>0)
+      {
+        res=insert(res,carry);
+      }
+      return res;
+}
+void display(struct node *head)
+{
+  struct node *temp;
+ temp=head;
+ temp=temp->next;//remove header node
+  while(temp!=head)//temp->next!=head;WHY?
+  {
+    printf("%d",temp->data);
+    temp=temp->next;
+  }
+  printf("\n");
+}
